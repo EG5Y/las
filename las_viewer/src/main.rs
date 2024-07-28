30ff310502f57657ae01485588e3408f0fd791d5
Your Name <you@example.com>
2024-07-28 15:37:56 +0300

    octree cube

--- las_viewer/src/main.rs
+++ las_viewer/src/main.rs
@@ -1,11 +1,19 @@
-use std::f32::consts::PI;
+use std::{collections::VecDeque, f32::consts::PI};
 
 use bevy::{
     color::palettes::basic::SILVER,
-    pbr::{wireframe::{WireframeConfig, WireframePlugin}, MaterialPipeline, MaterialPipelineKey},
+    pbr::{
+        wireframe::{WireframeConfig, WireframePlugin},
+        MaterialPipeline, MaterialPipelineKey,
+    },
     prelude::*,
     render::{
-        mesh::MeshVertexBufferLayoutRef, render_asset::RenderAssetUsages, render_resource::{AsBindGroup, Extent3d, PolygonMode, RenderPipelineDescriptor, ShaderRef, SpecializedMeshPipelineError, TextureDimension, TextureFormat}
+        mesh::MeshVertexBufferLayoutRef,
+        render_asset::RenderAssetUsages,
+        render_resource::{
+            AsBindGroup, Extent3d, PolygonMode, RenderPipelineDescriptor, ShaderRef,
+            SpecializedMeshPipelineError, TextureDimension, TextureFormat,
+        },
     },
 };
 use bevy_panorbit_camera::{PanOrbitCamera, PanOrbitCameraPlugin};
@@ -17,6 +25,137 @@ struct LineMaterial {
     #[uniform(0)]
     color: LinearRgba,
 }
+
+#[derive(Debug, Clone)]
+struct Octree {
+    root: OctreeCube, // pub points: [[f32; 3]; 8],
+                      // nodes: [OctreeCube; 8]
+}
+
+#[derive(Debug, Clone)]
+struct OctreeCube {
+    points: [[f32; 3]; 8],
+    nodes: Option<Box<[OctreeCube; 8]>>,
+}
+
+impl Octree {
+    pub fn new(size: f32) -> Self {
+        let root = octree_gen_points(0.0, 0.0, 0.0, size);
+
+        
+        let mut nodes = OctreeCube::into_cluster(root);
+        nodes[0].divide();
+        nodes[0].nodes.as_mut().unwrap()[0].divide();
+
+
+        Self {
+            root: OctreeCube {
+                points: root,
+                nodes: Some(Box::new(nodes)),
+            },
+        }
+    }
+}
+
+impl OctreeCube {
+    pub fn divide(&mut self) {
+        self.nodes = Some(Box::new(OctreeCube::into_cluster(self.points)));
+    }
+
+    pub fn into_cluster(cube: [[f32; 3]; 8]) -> [OctreeCube; 8] {
+        let cluster = octree_divide(&cube);
+        OctreeCube::from_cluster(cluster)
+    }
+
+    pub fn from_cluster(points: [[[f32; 3]; 8]; 8]) -> [OctreeCube; 8] {
+        [
+            OctreeCube { points: points[0], nodes: None },
+            OctreeCube { points: points[1], nodes: None },
+            OctreeCube { points: points[2], nodes: None },
+            OctreeCube { points: points[3], nodes: None },
+
+            OctreeCube { points: points[4], nodes: None },
+            OctreeCube { points: points[5], nodes: None },
+            OctreeCube { points: points[6], nodes: None },
+            OctreeCube { points: points[7], nodes: None },
+        ]
+    }
+}
+
+fn octree_gen_points(x: f32, y: f32, z: f32, size: f32) -> [[f32; 3]; 8] {
+    [
+        [x, y, z],
+        [x, y+size, z],
+        [x+size, y+size, z],
+        [x+size, y, z],
+        [x, y, z+size],
+        [x, y+size, z+size],
+        [x+size, y+size, z+size],
+        [x+size, y, z+size],
+    ]
+}
+
+fn octree_divide(octree_cube: &[[f32; 3]; 8]) -> [[[f32; 3]; 8]; 8] {
+    let [start_x, start_y, start_z] = octree_cube[0];
+    let [width_x, width_y, width_z] = octree_cube[3];
+    let [height_x, height_y, height_z] = octree_cube[1];
+    let [len_x, len_y, len_z] = octree_cube[4];
+
+    let size = (width_x - start_x) / 2.0;
+    let middle_x = start_x + size;
+    let middle_y = start_y + size;
+    let middle_z = start_z + size;
+
+    // let middle_x = start_x + (width_x - start_x) / 2.0;
+    // let middle_y = start_y + (height_y - start_y) / 2.0;
+    // let middle_z = start_z + (len_z - start_z) / 2.0;
+
+    // let [width_half_x, width_half_y, width_half_z] = [
+    //     start_x + (width_x - start_x) / 2.0,
+    //     start_y + (width_y),
+    //     width_z / 2.0,
+    // ];
+    // let [height_half_x, height_half_y, height_half_z] =
+    //     [height_x / 2.0, height_y / 2.0, height_z / 2.0];
+    // let [len_half_x, len_half_y, len_half_z] = [len_x / 2.0, len_y / 2.0, len_z / 2.0];
+
+    [
+        octree_gen_points(start_x, start_y, start_z, size),
+        octree_gen_points(start_x, middle_y, start_z, size),
+        octree_gen_points(middle_x, middle_y, start_z, size),
+        octree_gen_points(middle_x, start_y, start_z, size),
+
+        octree_gen_points(start_x, start_y, middle_z, size),
+        octree_gen_points(start_x, middle_y, middle_z, size),
+        octree_gen_points(middle_x, middle_y, middle_z, size),
+        octree_gen_points(middle_x, start_y, middle_z, size),
+
+    ]
+}
+
+// impl Octree {
+//     pub fn new(height: f32, width: f32, length: f32) -> Self {
+//         Self {
+//             points: [
+//                 [0.0, 0.0, 0.0],
+//                 [0.0, height, 0.0],
+//                 [width, height, 0.0],
+//                 [width, 0.0, 0.0],
+//                 [0.0, 0.0, length],
+//                 [0.0, height, length],
+//                 [width, height, length],
+//                 [width, 0.0, length],
+//             ],
+//         }
+//     }
+// }
+
+enum OctreeNode {
+    Sphere,
+    Cube,
+}
+
+struct OctreeSphere {}
 
 fn main() {
     App::new()
@@ -44,7 +183,9 @@ fn setup(
 ) {
     //bevy::pbr::wireframe::WireframeMaterial::default();
     //let debug_material = materials.add(StandardMaterial::from_color(Color::linear_rgba(0., 1., 0., 1.0)));
-    let debug_material = line_materials.add(LineMaterial { color: LinearRgba::GREEN });
+    let debug_material = line_materials.add(LineMaterial {
+        color: LinearRgba::GREEN,
+    });
 
     let (points, header) = read_las("/home/hey/Downloads/2743_1234.las");
     let bounds = header.bounds();
@@ -52,22 +193,30 @@ fn setup(
     let mut modified_points = octotree(&points, &bounds);
     drop(points);
 
-    transform(&mut modified_points, &bounds);
+    transform_move(&mut modified_points, &bounds);
 
-    let mesh = Mesh::new(
+    let mesh: Mesh = Mesh::new(
         bevy::render::mesh::PrimitiveTopology::PointList,
         RenderAssetUsages::default(),
     )
     .with_inserted_attribute(Mesh::ATTRIBUTE_POSITION, modified_points);
 
-    let lines = Mesh::new(
-        bevy::render::mesh::PrimitiveTopology::LineList,
-        RenderAssetUsages::default(),
-    )
-    //.with_inserted_attribute(Mesh::ATTRIBUTE_NORMAL, vec![[1.0, 1.0, 1.0], [1.0, 1.0, 1.0]])
-    //.with_inserted_attribute(Mesh::ATTRIBUTE_UV_0, vec![[0.0, 1.0], [0.5, 0.0], [1.0, 0.0], [0.5, 1.0]])
-    .with_inserted_indices(bevy::render::mesh::Indices::U32(vec![0, 1]))
-    .with_inserted_attribute(Mesh::ATTRIBUTE_POSITION, vec![[0.0, 0.0, 1000.0]]);
+    let size = [
+        bounds.max.x - bounds.min.x,
+        bounds.max.y - bounds.min.y,
+        bounds.max.z - bounds.min.z,
+    ]
+    .into_iter()
+    .max_by(|a, b| a.partial_cmp(b).unwrap())
+    .unwrap() as f32;
+    println!("octree size: {}", size);
+    //let size = [bounds.max.x, bounds.max.y , bounds.max.z].into_iter().max().unwrap() as f32;
+
+    //max!
+
+    let tree = Octree::new(size);
+    println!("{:#?}", tree);
+    let lines = gen_debug_lines(&tree);
 
     let mesh = meshes.add(mesh);
     let lines = meshes.add(lines);
@@ -75,8 +224,8 @@ fn setup(
     commands.spawn((
         PbrBundle {
             mesh: mesh,
-            transform: Transform::from_xyz(0.0, 0.0, 0.0)
-              .with_rotation(Quat::from_rotation_x(45.0)),
+            transform: Transform::from_xyz(0.0, 0.0, 0.0),
+            //  .with_rotation(Quat::from_rotation_x(45.0)),
             ..default()
         },
         Shape,
@@ -88,7 +237,7 @@ fn setup(
             material: debug_material,
             //material: debug_material,
             transform: Transform::from_xyz(0.0, 0.0, 0.0),
-             // .with_rotation(Quat::from_rotation_x(45.0)),
+            // .with_rotation(Quat::from_rotation_x(45.0)),
             ..default()
         },
         Shape,
@@ -127,19 +276,17 @@ fn setup(
 }
 
 fn read_las(path: &str) -> (Vec<[f32; 3]>, las::header::Header) {
-    
     let mut reader = las::Reader::from_path(path).unwrap();
     let header = las::Read::header(&reader).clone();
     println!("{header:#?}");
-
-    
 
     let cache_path = std::path::Path::new(CACHE_PATH);
     let points = if cache_path.exists() {
         println!("reading cached points...");
         let points = std::fs::read(CACHE_PATH).expect("faild to read cache file, delete it");
         println!("deserializing cached points...");
-        let points = bincode::deserialize::<Vec<[f32; 3]>>(&points).expect("failed to deserialize cache file, delete it");
+        let points = bincode::deserialize::<Vec<[f32; 3]>>(&points)
+            .expect("failed to deserialize cache file, delete it");
         println!("reading/deserializing completed");
         points
     } else {
@@ -149,13 +296,13 @@ fn read_las(path: &str) -> (Vec<[f32; 3]>, las::header::Header) {
 
         for wrapped_point in las::Read::points(&mut reader) {
             let point = wrapped_point.unwrap();
-    
-            points.extend([[point.x as f32, point.y as f32, point.z as f32]]);
-    
+
+            points.extend([[point.x as f32, point.z as f32, point.y as f32]]);
+
             if p_i % 100000 == 0 {
                 println!("reading points: {}", p_i);
             }
-    
+
             p_i += 1;
         }
         println!("finished reading points: {}", p_i);
@@ -165,7 +312,6 @@ fn read_las(path: &str) -> (Vec<[f32; 3]>, las::header::Header) {
         points
     };
 
-   
     let result = (points, header);
 
     result
@@ -173,7 +319,6 @@ fn read_las(path: &str) -> (Vec<[f32; 3]>, las::header::Header) {
 
 fn octotree(points: &[[f32; 3]], bounds: &las::Bounds) -> Vec<[f32; 3]> {
     let mut modified_pos = Vec::with_capacity(points.len());
-    let mut p_i = 0;
 
     //let m = bounds.min.z;
     let min_x = bounds.min.x as f32;
@@ -191,59 +336,122 @@ fn octotree(points: &[[f32; 3]], bounds: &las::Bounds) -> Vec<[f32; 3]> {
     let len_x_middle = len_x / 2.0;
     let len_y_middle = len_y / 2.0;
     let len_z_middle = len_z / 2.0;
-
+    println!("running octree algo...");
     for [x, y, z] in points {
         // if *x > len_x_middle + min_x {
         //     continue;
         // }
 
-        
-    //   if
-    //   // p_i % 100 == 0 
-    //      (*x > len_x_middle + min_x
-    //     && *y > len_y_middle + min_y
-    //     && *z > len_z_middle + min_z)
-    //      ||
+        //   if
+        //   // p_i % 100 == 0
+        //      (*x > len_x_middle + min_x
+        //     && *y > len_y_middle + min_y
+        //     && *z > len_z_middle + min_z)
+        //      ||
 
-    //      (*x < len_x_middle + min_x
-    //         && *y > len_y_middle + min_y
-    //         && *z > len_z_middle + min_z)
-        
-    //     {
-    //         modified_pos.extend([[*x, *y, *z]]);
-    //     }
+        //      (*x < len_x_middle + min_x
+        //         && *y > len_y_middle + min_y
+        //         && *z > len_z_middle + min_z)
+
+        //     {
+        //         modified_pos.extend([[*x, *y, *z]]);
+        //     }
         modified_pos.extend([[*x, *y, *z]]);
-
-        if p_i % 100000 == 0 {
-            println!("octotree points: {}", p_i);
-        }
-        p_i += 1;
     }
 
-    println!("finished octotree'ing points: {}", p_i);
+    println!("finished octree algo");
 
     modified_pos
 }
 
-fn transform(points: &mut [[f32; 3]], bounds: &las::Bounds) {
-    let mut p_i = 0;
-
+fn transform_center(points: &mut [[f32; 3]], bounds: &las::Bounds) {
+    println!("transforming points...");
     for [x, y, z] in points.iter_mut() {
         *x = (*x - bounds.min.x as f32) - ((bounds.max.x as f32 - bounds.min.x as f32) / 2.0);
         *y = (*y - bounds.min.y as f32) - ((bounds.max.y as f32 - bounds.min.y as f32) / 2.0);
         *z -= bounds.min.z as f32;
-
-        if p_i % 100000 == 0 {
-            println!("modifying points: {}", p_i);
-        }
-
-        p_i += 1;
     }
-
-    println!("finished modifying points: {}", p_i);
+    println!("finished transforming points.");
 }
 
+fn transform_move(points: &mut [[f32; 3]], bounds: &las::Bounds) {
+    println!("transforming points...");
 
+    for [x, y, z] in points.iter_mut() {
+        *x -= bounds.min.x as f32;
+        *y -= bounds.min.z as f32;
+        *z -= bounds.min.y as f32;
+    }
+
+    println!("finished transforming points.");
+}
+
+fn gen_debug_lines(tree: &Octree) -> Mesh {
+    //let pos = tree.root.points.to_vec();
+    let mut pos: Vec<[f32; 3]> = Vec::new();
+    let mut indices: Vec<u32> = Vec::new();
+
+    let mut queue = VecDeque::<&OctreeCube>::new();
+
+    queue.push_back(&tree.root);
+    // if let Some(nodes) = &tree.root.nodes {
+    //     //queue.push_back(&nodes[0]) ;
+    //     for node in nodes.iter().skip(1) {
+    //         queue.push_back(node);
+    //     }
+    // }
+    
+
+    let mut i: u32 = 0;
+    loop {
+        let Some(cube) = queue.pop_front() else {
+            break;
+        };
+
+        #[rustfmt::skip]
+        let new_indices: [u32; 24]  = [
+            i, i + 1,
+            i + 1, i + 2,
+            i + 2, i + 3,
+            i, i + 3,
+    
+            i, i + 4,
+            i + 1, i + 5,
+            i + 2, i + 6,
+            i + 3, i + 7,
+    
+            i + 4, i + 5,
+            i + 5, i + 6,
+            i + 6, i + 7,
+            i + 4, i + 7,
+        ];
+
+        pos.extend_from_slice(&cube.points);
+        indices.extend(new_indices);
+
+        if let Some(nodes) = &cube.nodes {
+            for node in nodes.iter() {
+                queue.push_back(node);
+            }
+        }
+
+        i += 8;
+    }
+
+    
+   // let indices = vec!;
+
+    let lines = Mesh::new(
+        bevy::render::mesh::PrimitiveTopology::LineList,
+        RenderAssetUsages::default(),
+    )
+    //.with_inserted_attribute(Mesh::ATTRIBUTE_NORMAL, vec![[1.0, 1.0, 1.0], [1.0, 1.0, 1.0]])
+    //.with_inserted_attribute(Mesh::ATTRIBUTE_UV_0, vec![[0.0, 1.0], [0.5, 0.0], [1.0, 0.0], [0.5, 1.0]])
+    .with_inserted_indices(bevy::render::mesh::Indices::U32(indices))
+    .with_inserted_attribute(Mesh::ATTRIBUTE_POSITION, pos);
+
+    lines
+}
 
 impl Material for LineMaterial {
     fn fragment_shader() -> ShaderRef {
